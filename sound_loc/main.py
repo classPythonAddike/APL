1aad65e038193d0a87a1c6507179c96c7727fd2e
Keshav <72556571+classPythonAddike@users.noreply.github.com.>
2024-12-22 06:22:18 +0530

    Sound localisation

---
+++ sound_loc/main.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+Nmics = 64
+Nsamp = 200
+
+src = (0, 0)
+
+pitch = 0.1
+dist_per_samp = 0.1
+C = 2.0
+SincP = 5.0
+
+mics = []
+
+obstacle = (3, -1)
+
+# assert Nmics % 2 == 0, "Number of mics must be even!"
+
+for i in range(Nmics):
+    mics.append((0, pitch * (-Nmics // 2 + 0.5 + i)))
+
+t = np.linspace(0, Nsamp * dist_per_samp / C, Nsamp)
+def wsrc(t):
+    return np.sinc(SincP * t)
+
+wave = wsrc(t)
+
+def dist(src, pt, mic):
+    d1 = ((src[1] - pt[1]) ** 2 + (src[0] - pt[0]) ** 2) ** 0.5
+    d2 = ((mic[1] - pt[1]) ** 2 + (mic[0] - pt[0]) ** 2) ** 0.5
+    return d1 + d2
+
+mic_output = np.zeros((Nmics, Nsamp))
+
+for mic in range(Nmics):
+    mic_output[mic] = wsrc(t - dist(src, obstacle, mics[mic]) / C)
+
+graph_x = np.arange(Nsamp)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('_mpl-gallery')
+
+
+colors = "bgrcm"
+
+#for i in range(Nmics):
+    #plt.plot(graph_x, mic_output[i] + 0.5 * i, c=colors[i % len(colors)], linewidth=3)
+plt.imshow(mic_output, cmap="viridis")
+
+plt.show()
+
+reconst_img = np.zeros((Nmics, Nsamp))
+
+for x in range(Nsamp):
+    for y in range(Nmics):
+        for mic_ind, mic in enumerate(mics):
+            t_delay = round(dist(src, (dist_per_samp * x, pitch * (y - Nmics / 2 + 0.5)), mic) / dist_per_samp)
+            if t_delay < Nsamp:
+                reconst_img[y, x] += mic_output[mic_ind][t_delay]
+
+plt.imshow(reconst_img, cmap="viridis")
+plt.colorbar()
+plt.show()
